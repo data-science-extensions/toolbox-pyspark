c91371e3b0ba37016c33bec21ed1eebc1982ad20
Chris Mahoney <44449504+chrimaho@users.noreply.github.com>
2024-12-12 18:28:11 +1100

    Fix typos

--- src/toolbox_pyspark/types.py
+++ src/toolbox_pyspark/types.py
@@ -50,12 +50,9 @@ from toolbox_python.collection_types import str_list
 from toolbox_python.dictionaries import dict_reverse_keys_and_values
 from typeguard import typechecked
 
-# ## Local Module Imports ----
-from .checks import (
-    assert_column_exists,
-    assert_columns_exists,
-)
-from .constants import (
+# ## Local First Party Imports ----
+from toolbox_pyspark.checks import assert_column_exists, assert_columns_exists
+from toolbox_pyspark.constants import (
     VALID_DATAFRAME_NAMES,
     VALID_PYSPARK_DATAFRAME_NAMES,
 )
@@ -66,7 +63,7 @@ from .constants import (
 # ---------------------------------------------------------------------------- #
 
 
-__all__: list[str] = [
+__all__: str_list = [
     "get_column_types",
     "cast_column_to_type",
     "cast_columns_to_type",
